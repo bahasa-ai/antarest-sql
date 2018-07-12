12c8687b65bcbdda80cf4af5699c287b46a9c132
M Gilang Januar <mgilangjanuar@gmail.com>
2018-07-12 14:01:35 +0700

    add example

---
+++ example/cat.js
@@ -0,0 +1,30 @@
+var express = require('express')
+var Sequelize = require('sequelize')
+var bodyParser = require('body-parser')
+
+var antarest = require('../lib/antarestSql')
+
+var app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+var Cat = { name: Sequelize.TEXT, weight: Sequelize.INTEGER }
+
+app.use(
+  antarest(
+    'sqlite://cat.sqlite', 
+    {}, 
+    [
+      { 
+        path: '/cat', 
+        model: {
+          name: 'Cat',
+          schema: Cat
+        }
+      }
+    ]
+  )
+)
+
+app.listen(6969)
